04831159ee4356f24bafe4fc758d2a7798511fd8
Vivek Shahi <shahivivek503@gmail.com>
2026-02-14 21:19:21 +0530

    Rewrite code 14-02--26

--- set_metrix.java
+++ set_metrix.java
@@ -67,5 +67,60 @@ class  solution{
 }
 
 
+//***OPTIMIZED SOLUTION****/
+class Solution{
+    public void setMatrix(int[][] matrix){
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for(int j=0;j<col;j++){
+            if(matrix[0][j]==0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        if(firstColZero){
+            for(int i=0;i<row;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+
+        if(firstRowZero){
+            for(int j=0;j<col;j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
 
 
