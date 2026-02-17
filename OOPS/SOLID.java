0df72665aa1fb37b523ccebc5c6999a1c5bf7993
Vivek Shahi <shahivivek503@gmail.com>
2026-02-17 09:24:38 +0530

    DATE- 17-02-2026

---
+++ OOPS/SOLID.java
@@ -0,0 +1,8 @@
+public class SOLID {
+    
+}
+//Single resposibility Principle
+//Open/close principle
+//Leskiove Substution Principle
+//Interface Segregation principle
+//Dependency inversion principle
---
+++ OOPS/pillar.java
@@ -0,0 +1,15 @@
+// Java is not 100% object orientes programming language because it has some primitive datatype
+// like int, char, boolean etc which are not objects but java provides wrapper classes for these primitive datatypes
+// like Integer, Charaacter , Boolean etc which are objects and can be used in place of primitive datatypes when needed.
+// and java have static keyword which is used to create static members of a class which can be accessed without 
+// creating an object of the class.
+public class pillar {
+    
+}
+
+//Encapsulation -geter and setter
+//Abstraction -abstract class and interface
+//Inheritance - super and sub class
+//Polymorphism -method overloading and method overriding
+
+
